18aa4f35a212040840a907fb12b6336d6f9fba67
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2021-10-26 16:38:32 +0530

    Custom Menus

--- functions.php
+++ functions.php
@@ -2,7 +2,26 @@
 
 /*
  ========================================
-        Functions File
+    Add Custom Menus
+ ========================================
+*/
+//add_theme_support( 'menus' );
+
+if ( ! function_exists( 'awesometheme_register_nav_menu' ) ) {
+ 
+    function awesometheme_register_nav_menu(){
+        register_nav_menus( array(
+            'primary_menu' => __( 'Primary Menu', 'text_domain' ),
+            //'footer_menu'  => __( 'Footer Menu', 'text_domain' ),
+        ) );
+    }
+    add_action( 'after_setup_theme', 'awesometheme_register_nav_menu', 0 );
+}
+
+
+/*
+ ========================================
+        Enqueue Style and Script Files
  ========================================
 */
 
@@ -11,3 +30,5 @@ function awesome_theme_scripts() {
     wp_enqueue_script( 'customscript', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'awesome_theme_scripts' );
+
+
--- header.php
+++ header.php
@@ -6,4 +6,9 @@
         <?php wp_head(); ?>
     </head>
     <body>
+
+    <?php wp_nav_menu( array(
+            'theme_location' => 'primary_menu',
+        ) );
+?>
     <p>This is the header Section</p>
