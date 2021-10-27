39e7e000434d2217bf5b8a9da230e4704e9dc922
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2021-10-27 22:51:37 +0530

    wp query with category

--- index.php
+++ index.php
@@ -1,17 +1,51 @@
 <?php get_header(); ?>
     <div class="row">
             <div class="col-xs-12 col-sm-8">
+            <?php 
+                // the query
+                $cat = array(4,5);
+                foreach($cat as $a){
+                $args = array(
+                    'post_type' => 'post',
+                    'post_per_page' => 3,
+                    'category__in' => $a,
+                    'category__not_in' => 1 
+                );
+                $the_query = new WP_Query( $args ); ?>
+                
+                <?php if ( $the_query->have_posts() ) : ?>
+                
+                    <!-- pagination here -->
+                
+                    <!-- the loop -->
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                    <?php endwhile; ?>
+                    <!-- end of the loop -->
+                
+                    <!-- pagination here -->
+                
+                    <?php wp_reset_postdata(); ?>
+                
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                <?php endif; 
+                
+                }?>
+
+
+
 
             <?php 
-            if ( have_posts() ) {
-                while ( have_posts() ) {
-                    the_post();  ?>
+            // if ( have_posts() ) {
+            //     while ( have_posts() ) {
+            //         the_post();  ?>
                     
-                    <?php get_template_part('content',get_post_format()); ?>     
-                <hr>
+                    <?php // get_template_part('content',get_post_format()); ?>     
+                 <!-- <hr> -->
 
-            <?php 	} // end while
-            } // end if
+            <?php  //	} // end while
+            // } // end if
             ?>
             </div>
 
