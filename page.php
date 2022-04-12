c360ef5ae804c83ad5868d044dea3761c0e66d5f
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-12 23:11:03 +0530

    page

---
+++ page.php
@@ -0,0 +1,24 @@
+
+<?php get_header(); ?>
+
+
+<h1>Here</h1>
+<script>
+    //createInput()
+    </script>
+
+
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();  ?>
+        Posted on: <?php the_time('F j, Y');?> in <?php the_category();?>
+     <h1><?php the_title(); ?> </h1> 
+     
+     <?php the_content(); ?>
+     
+     <hr>
+<?php 	} // end while
+} // end if
+?>
+<?php get_footer(); ?>
